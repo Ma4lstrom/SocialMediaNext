16ea678a3efa9c47e3758688c6f6d280b7902ffc
A Stevens <146134930+AStevens-Ai@users.noreply.github.com>
2026-04-07 20:34:40 -0400

    removed use server from a component

--- src/app/components/FetchUserHeader.jsx
+++ src/app/components/FetchUserHeader.jsx
@@ -1,16 +1,15 @@
 // src/app/components/FetchUserHeader.jsx
-'use server'
 import { checkUser } from '../lib/checkUser'; // Server-side logic
 import RootLayout from '../layout'; // The RootLayout component (Client-side)
 
 export default async function FetchUserHeader({ children }) {
     // Fetch user data on the server
-    const user = await checkUser();
-    console.log('User in FetchUserHeader:', user);
+    // const user = await checkUser();
+    // console.log('User in FetchUserHeader:', user);
 
     // Return the RootLayout component and pass the user data as props
     return (
-        <RootLayout user={user}>
+        <RootLayout>
             {children}
         </RootLayout>
     );
